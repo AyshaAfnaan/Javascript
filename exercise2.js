2883363f04a1f904377fc43ad36aff7bd9a3c666
AyshaAfnaan <ayshaafnaan23@gmail.com>
2023-10-08 23:13:38 +0530

    extra work

---
+++ exercise2.js
@@ -0,0 +1,98 @@
+// Additional assignments for Day 3
+
+/*EX 1
+ Use a ternary operator to assign to a variable called gender the string values "male" or "female".
+ The choice should be made based on the value of another variable called isMale.
+*/
+
+console.log("\n-------------Exercise 1-------------\n");
+
+let gender;
+let isMale = true
+(isMale === gender)
+ "true" : "false";
+ console.log ("result:", isMale);
+
+/*EX 2
+ Write a piece of code for checking if, given two integers, the value of one of them is 8 or if their addition or subtraction is equal to 8.
+*/
+
+console.log("\n-------------Exercise 2-------------\n");
+
+let a= 8;
+let b =5;
+if((a===8)||(b===8)||(a+b===8)||(a-b===8)||(b-a===8)){
+console.log(true);
+}else{
+    console.log(false);
+}
+
+/*EX 3
+ Create a variable and assign to it the concatenation of two strings.
+*/
+
+console.log("\n-------------Exercise 3-------------\n");
+let string1 = "aysha";
+let string2 = "afnaan";
+let fullName = string1 + " " + string2;
+console.log ("Concatenation of two strings:", fullName);
+
+
+/*EX 4
+ Create three variables and assign a numerical value to each one of them. 
+ Using a conditional statement, write a piece of code for sorting their values from highest to lowest.
+ Display the result in the console.
+*/
+
+console.log("\n-------------Exercise 4-------------\n");
+
+let x = 
+
+/*EX 5
+ Write a piece of code for finding the average of two given integers.
+*/
+
+console.log("\n-------------Exercise 5-------------\n");
+
+let maths =50;
+let science =70;
+let average = (maths+science)/2;
+console.log("average:", average);
+
+
+/*EX 6
+ Write a piece of code for finding the longest of two given strings.
+*/
+
+console.log("\n-------------Exercise 6-------------\n");
+
+
+/*EX 7
+ Write a piece of code for checking if a given value is a integer or not.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/*EX 8
+ Write a piece of code for calculating a certain percentage of a given number.
+ (Ex.: the 20% of 400 is 80)
+*/
+
+console.log("\n-------------Exercise 8-------------\n");
+let divisionOf = 400;
+let divedBy = 20
+let percentage = divisionOf % divedBy;
+console.log ("Percentage of given numbers", percentage); 
+
+/*EX 9
+ Write a piece of code for checking if a given number is even or odd.
+*/
+
+console.log("\n-------------Exercise 3-------------\n");
+let aNumber = 2;
+let givenNumberIsOddOrEven;
+if(aNumber % 2 ===0){
+    console.log("It is even number");
+}else{
+    console.log("it is a odd number");
+}
